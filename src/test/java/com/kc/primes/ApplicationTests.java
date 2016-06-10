c1633332219ec0762eee1c7a55ecc934cf383ae5
Kishor Chukka <kishorechk@gmail.com>
2016-06-10 15:22:59 +0100

    Update ApplicationTests.java

--- src/test/java/com/kc/primes/ApplicationTests.java
+++ src/test/java/com/kc/primes/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.kc.primes;
 
+import com.kc.primes.main.PrimeGeneratorServiceMain;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
