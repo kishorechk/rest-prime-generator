7761fc84458a7ea8dedd2756c90e45835bc25034
Kishor Chukka <kishorechk@gmail.com>
2016-06-10 12:08:28 +0100

    Create PrimeGeneratorServiceMain.java

---
+++ src/main/java/com/kc/primes/main/PrimeGeneratorServiceMain.java
@@ -0,0 +1,16 @@
+package com.kc.primes.main;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.kc.primes.PrimeGeneratorServiceConfiguration;
+import org.springframework.boot.SpringApplication;
+
+import java.util.concurrent.TimeUnit;
+
+public class PrimeGeneratorServiceMain {
+    static final MetricRegistry metrics = new MetricRegistry();
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeGeneratorServiceConfiguration.class, args);
+    }
+}
